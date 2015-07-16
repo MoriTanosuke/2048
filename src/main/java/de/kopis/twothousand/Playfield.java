f67b3af8d6f3e45e179362a4d1b6841ed16e3255
Carsten <carsten@kopis.de>
2015-07-16 07:14:21 +0200

    Implement UP/DOWN.
    Still a very conservative/java7 implementation. I want to finish the game first, before trying the new stuff. :)

--- src/main/java/de/kopis/twothousand/Playfield.java
+++ src/main/java/de/kopis/twothousand/Playfield.java
@@ -1,10 +1,22 @@
 package de.kopis.twothousand;
 
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.logging.Logger;
 
 public class Playfield {
+    private static final Logger logger = Logger.getLogger(Playfield.class.getName());
+
+    public enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+
     private final int maxX;
     private final int maxY;
     private List<Tile> tiles = new ArrayList<Tile>();
@@ -18,15 +30,104 @@ public class Playfield {
         Random r = new Random(System.nanoTime());
         int x = 1 + r.nextInt(maxX - 1);
         int y = 1 + r.nextInt(maxY - 1);
+        logger.info(String.format("Adding random tile to %d,%d", x, y));
         addTile(new Tile(x, y, 2));
     }
 
     public boolean hasAvailableSlots() {
-        System.out.println(tiles.size() + " <? " + maxX * maxY);
+        logger.info(String.format("Placed tiles: %d, Max: %d", tiles.size(), maxX * maxY));
         return tiles.size() <= maxX * maxY;
     }
 
-    private void addTile(Tile tile) {
+    public void addTile(Tile tile) {
+        logger.info(String.format("Adding tile value=%d at %d,%d", tile.value, tile.x, tile.y));
         tiles.add(tile);
+    }
+
+    public void moveTile(int x, int y, Direction direction) {
+        Tile tile = getTile(x, y);
+
+        if (tile == null) {
+            // no tile found, nothing to move
+            return;
+        }
+
+        // tile matched, now move to direction
+        switch (direction) {
+            case UP:
+                move(x, y, x, y - 1);
+                break;
+            case DOWN:
+                move(x, y, x, y + 1);
+                break;
+            case LEFT:
+                break;
+            case RIGHT:
+                break;
+        }
+    }
+
+    private void move(int x, int y, int newX, int newY) {
+        Tile thisTile = getTile(x, y);
+        int newValue = thisTile != null ? thisTile.value : 0;
+
+        Tile otherTile = getTile(newX, newY);
+        if (otherTile != null) {
+            if (thisTile.value == otherTile.value) {
+                newValue = thisTile.value + otherTile.value;
+                removeTile(x, y);
+                addTile(new Tile(newX, newY, newValue));
+            }
+        } else {
+            removeTile(x, y);
+            addTile(new Tile(newX, newY, newValue));
+        }
+    }
+
+    private Tile removeTile(int x, int y) {
+        for (Tile t : tiles.toArray(new Tile[0])) {
+            if (t.x == x && t.y == y) {
+                logger.info(String.format("Removing tile at %d,%d", t.x, t.y));
+                tiles.remove(t);
+                return t;
+            }
+        }
+        return null;
+    }
+
+    public Tile getTile(int x, int y) {
+        Tile tile = null;
+        for (Tile t : tiles) {
+            if (t.x == x && t.y == y) {
+                tile = t;
+            }
+        }
+        if (tile != null) {
+            logger.info(String.format("Tile value=%d at %d,%d", tile.value, tile.x, tile.y));
+        } else {
+            logger.info(String.format("No tile found at %d,%d", x, y));
+        }
+        return tile;
+    }
+
+    /**
+     * Prints the whole playfield using the given {@link Writer}.
+     *
+     * @param out {@link Writer} to use for printing. Is flushed after playfield is written.
+     * @throws IOException
+     */
+    public void print(Writer out) throws IOException {
+        for (int x = 0; x < maxX; x++) {
+            for (int y = 0; y < maxY; y++) {
+                Tile t = getTile(x, y);
+                if (t == null) {
+                    out.write("X");
+                } else {
+                    out.write(Integer.toString(t.value));
+                }
+            }
+            out.write("\n");
+        }
+        out.flush();
     }
 }
--- src/test/java/de/kopis/twothousand/PlayfieldTest.java
+++ src/test/java/de/kopis/twothousand/PlayfieldTest.java
@@ -2,7 +2,13 @@ package de.kopis.twothousand;
 
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
 import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class PlayfieldTest {
@@ -12,15 +18,67 @@ public class PlayfieldTest {
 
         final Playfield grid = new Playfield(size);
         assertTrue(grid.hasAvailableSlots());
-        grid.addRandomTile();
-        assertTrue(grid.hasAvailableSlots());
 
         // fill the whole grid with random tiles
-        for (int i = 1; i <= (size * size) - 1; i++) {
-            grid.addRandomTile();
-            assertTrue(grid.hasAvailableSlots());
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                grid.addTile(new Tile(x, y, 2));
+                assertTrue(grid.hasAvailableSlots());
+            }
         }
         grid.addRandomTile();
         assertFalse(grid.hasAvailableSlots());
+    }
+
+    @Test
+    public void playfieldCanMoveATileToEmptySpaces() {
+        final Playfield grid = new Playfield(4);
+        Tile originalTile = new Tile(4, 4, 2);
+        grid.addTile(originalTile);
+        grid.moveTile(4, 4, Playfield.Direction.UP);
+        Tile t1 = grid.getTile(4, 3);
+        assertEquals(originalTile.value, t1.value);
+        assertTrue(grid.getTile(4, 4) == null);
+
+        grid.moveTile(4, 3, Playfield.Direction.DOWN);
+        Tile t2 = grid.getTile(4, 4);
+        assertEquals(originalTile.value, t2.value);
+        assertTrue(grid.getTile(4, 3) == null);
+    }
+
+    @Test
+    public void playfieldCanMoveATileOnAnotherTileWithSameValue() {
+        final Playfield grid = new Playfield(4);
+        grid.addTile(new Tile(4, 3, 2));
+        grid.addTile(new Tile(4, 4, 2));
+        grid.moveTile(4, 4, Playfield.Direction.UP);
+
+        Tile t = grid.getTile(4, 3);
+        assertEquals(4, t.value);
+        assertTrue(grid.getTile(4, 4) == null);
+    }
+
+    @Test
+    public void playfieldCanNotMoveATileOnAnotherTileWithDifferentValue() {
+        final Playfield grid = new Playfield(4);
+        grid.addTile(new Tile(4, 3, 4));
+        grid.addTile(new Tile(4, 4, 2));
+        grid.moveTile(4, 4, Playfield.Direction.UP);
+
+        Tile t1 = grid.getTile(4, 3);
+        assertEquals(4, t1.value);
+
+        Tile t2 = grid.getTile(4, 4);
+        assertEquals(2, t2.value);
+    }
+
+    @Test
+    public void canPrintAPlayfield() throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        final Writer out = new PrintWriter(bos);
+        final Playfield p = new Playfield(4);
+        p.addTile(new Tile(1, 2, 8));
+        p.print(out);
+        assertEquals("XXXX\nXX8X\nXXXX\nXXXX\n", bos.toString());
     }
 }
