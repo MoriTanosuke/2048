6d1de6081dc141929d63080460a5f9f263963a25
Carsten <carsten@kopis.de>
2015-07-17 10:01:05 +0200

    Improve the unit test with more asserts to reveal the bug.

--- src/test/java/de/kopis/twothousand/GameTest.java
+++ src/test/java/de/kopis/twothousand/GameTest.java
@@ -28,6 +28,7 @@ public class GameTest {
         final AsciiPlayfield ascii = new AsciiPlayfield();
         final Playfield p = new Playfield(4);
 
+        // initial state
         p.addTile(new Tile(0, 1, 2));
         p.addTile(new Tile(0, 2, 8));
         p.addTile(new Tile(0, 3, 8));
@@ -40,29 +41,27 @@ public class GameTest {
         // X X 2 2
         // X X X 4
         // X X X 2
-        System.out.println();
-        ascii.print(new PrintWriter(System.out), p, controls);
-        p.move(Direction.RIGHT);
+        final ByteArrayOutputStream bos1 = new ByteArrayOutputStream();
+        ascii.print(bos1, p, controls);
+        assertEquals("   X   2   8   8\n   X   X   2   2\n   X   X   X   4\n   X   X   X   2\n\n", bos1.toString());
 
+        p.move(Direction.RIGHT);
         // X X 2 16
         // X X X 4
         // X X X 4
         // X X X 2
-        System.out.println("RIGHT");
-        ascii.print(new PrintWriter(System.out), p, controls);
+        final ByteArrayOutputStream bos2 = new ByteArrayOutputStream();
+        ascii.print(bos2, p, controls);
+        assertEquals("   X   X   2  16\n   X   X   X   4\n   X   X   X   4\n   X   X   X   2\n\n", bos2.toString());
 
+        p.move(Direction.DOWN);
         // X X X X
         // X X X 16
         // X X X 8
         // X X 2 2
-        p.move(Direction.DOWN);
-        System.out.println("DOWN");
-        ascii.print(new PrintWriter(System.out), p, controls);
-
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        ascii.print(new PrintWriter(bos), p, controls);
-        System.out.println(bos);
-        assertEquals("   X   X   X   X\n   X   X   X  16\n   X   X   X   8\n   X   X   2   2\n\n", bos.toString());
+        final ByteArrayOutputStream bos3 = new ByteArrayOutputStream();
+        ascii.print(bos3, p, controls);
+        assertEquals("   X   X   X   X\n   X   X   X  16\n   X   X   X   8\n   X   X   2   2\n\n", bos3.toString());
     }
 
 
